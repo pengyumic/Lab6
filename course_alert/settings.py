8ea1439288caed222b859b504dacfe8bc89dc8a7
Pengyu Chen <chen2087@purdue.edu>
2017-12-07 23:27:48 -0500

    changes

--- course_alert/settings.py
+++ course_alert/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-*9zxye=s0zz&sfovr43))_k2%@6+$o64v66nvk(m*d=n=rn)e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
