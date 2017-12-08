fbd85f1e7b7f7cc0ae1d59c71fd6d765320ca602
Pengyu Chen <chen2087@purdue.edu>
2017-12-07 22:35:42 -0500

    setup server config file

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn purduecoursealert.wsgi --log-file -
--- course_alert/settings.py
+++ course_alert/settings.py
@@ -120,5 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 LOGIN_REDIRECT_URL = '/userPage/'
 LOGIN_URL='/'
+import dj_database_url
+DATABASES = { 'default': dj_database_url.config() }
